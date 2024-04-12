f5eaf753b6608eef8cf9f6c8ad530ed59a72b110
Pedro Miguel Silva Cabrita <pesilva-@c1r4s2.42lisboa.com>
2024-04-12 17:47:36 +0100

    ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,39 @@
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	tuppercase(unsigned int d, char c)
+{
+	if (d % 2 == 0)
+	{
+		if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	}
+	return (c);
+}
+
+int main()
+{
+	char	*str;
+
+	str = ft_strmapi("ola caralho", tuppercase);
+	printf("string apos a funcao: %s", str);
+	free(str);
+	return (0);
+}
