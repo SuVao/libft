6e800d8168c494ff4b09389b14842958cece171c
SuVao <pedro.radon@gmail.com>
2024-04-13 13:41:35 +0100

    part2_done

---
+++ ft_striteri.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 13:30:27 by pesilva-          #+#    #+#             */
+/*   Updated: 2024/04/13 13:39:23 by pesilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+
+void print_index(unsigned int i, char *c)
+{
+	printf("Index: %d, Char: %c\n", i, *c);
+}
+
+int main()
+{
+	char	str[] = "ola caralho";
+
+	ft_striteri(str, print_index);
+	return (0);
+}
