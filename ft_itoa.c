f4cb288c2784869d8bdf9c345a3e8cf07f363852
SuVao <pedro.radon@gmail.com>
2024-04-13 12:50:08 +0100

    mapi

---
+++ ft_itoa.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 22:50:21 by pesilva-          #+#    #+#             */
+/*   Updated: 2024/04/12 22:50:21 by pesilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	count_dig(long n)
+{
+	int		count;
+
+	count = 0;
+	if (n < 0)
+	{
+		n = -n;
+		count++;
+	}
+	while (n > 9)
+	{
+		n = n / 10;
+		count++;
+	}
+	if (n < 10)
+	{
+		n = n % 10;
+		count++;
+	}
+	return (count);
+}
+
+static char	*convert(long nb, char *str, int count)
+{
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		str[count - 1] = (nb % 10) + '0';
+		nb /= 10;
+		count--;
+	}
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (count_dig(n) + 1));
+	if (!str)
+		return (0);
+	i = count_dig(n);
+	str = convert(n, str, i);
+	str[i] = '\0';
+	return (str);
+}
+
+int		main()
+{
+	char	*str;
+
+	str = ft_itoa(-2147483648);
+	printf("nbr = %s \n", str);
+	return (0);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 12:49:12 by pesilva-          #+#    #+#             */
+/*   Updated: 2024/04/13 12:49:12 by pesilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
