198e0070a6a66f400803e77f862e82183bb88ab9
SuVao <pedro.radon@gmail.com>
2024-04-17 15:31:50 +0100

    teste do lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:41:22 by pesilva-          #+#    #+#             */
-/*   Updated: 2024/04/17 12:41:31 by pesilva-         ###   ########.fr       */
+/*   Updated: 2024/04/17 15:26:46 by pesilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,42 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		lst = lst->next;
 	}
 	return (new_lst);
+}
+
+void *ft_rot32(void *content)
+{
+	char *str;
+	int i;
+
+	str = (char *)content;NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (content);
+}
+
+int main()
+{
+	t_list *list;
+	t_list *new_list;
+	t_list *tmp;
+
+	list = ft_lstnew(ft_strdup("Hello"));
+	list->next = ft_lstnew(ft_strdup("World"));
+	list->next->next = ft_lstnew(ft_strdup("42"));
+	list->next->next->next = ft_lstnew(ft_strdup("Lisboa"));
+	new_list = ft_lstmap(list, ft_rot32, free);
+	tmp = new_list;
+	while (tmp)
+	{
+		printf("%s\n", (char *)tmp->content);
+		tmp = tmp->next;
+	}
+	return (0);
 }
